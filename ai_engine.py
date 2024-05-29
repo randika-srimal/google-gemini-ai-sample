c03b58da223db1e20d3859f40f23491e5678b5e3
Randika Dissanayaka <email2randika@gmail.com>
2024-05-29 18:09:36 +0530

    WIP: fake hardcoded answer via python module

---
+++ ai_engine.py
@@ -0,0 +1,2 @@
+def generate_response(question):
+    return "I'm sorry, I don't know the answer to that question. Please try again."
---
+++ app.py
@@ -0,0 +1,5 @@
+import ai_engine
+ 
+question = "What cars are available?"
+ 
+print(ai_engine.generate_response(question))
