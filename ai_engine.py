f4d01888cdb92305793f4d0c135adc04426bdc19
Randika Dissanayaka <email2randika@gmail.com>
2024-05-30 12:20:43 +0530

    WIP: Good level of prompt

--- ai_engine.py
+++ ai_engine.py
@@ -40,51 +40,64 @@ ORDER BY table_name, ordinal_position
  
     return table_data
 
-def build_prompt(question, query=None, query_output=None, chat_messages=None):
+def build_answer_prompt(user_question, query, query_results):
+ 
+    return f"""
+    Context:
+    
+    Analyse the User Question, SQL query and SQL Result and generate a human friendly answer.
+    
+    User Question:
+    
+    {user_question}
+    
+    SQL Query:
+    
+    {query}
+    
+    SQL Result:
+    
+    {query_results}
+    
+    Deliverables:
+    
+    Generated human friendly answer.        
+    """
+    
+def build_query_prompt(question):
     tables = get_table_names()
  
     table_prompt = ""
     for table in tables:
         table_prompt += f"{table} has columns: " + ', '.join([f"{column['name']} ({column['type']})" for column in tables[table]]) + "\n"
- 
-    history = ""
     
-    if chat_messages and chat_messages != "":
-        history = "\n" + chat_messages
- 
-    query_prompt = ""
- 
-    if query:
-        query_prompt += "SQLQuery: " + query + "\n"
-        if query_output:
-            query_prompt += "SQLResult: " + query_output + "\n"
-        rules = "Answer: Above SQLResult: in a human friendly language"
-    else:
-        rules = ("(Your answer HERE must be a syntactically correct MySQL query with no extra information or "
-                 "quotes.Omit SQLQuery: from your answer)")
+    table_prompt+="level column of the employee_skills table can only contain Good, Bad as level. Table names and column names are lowercase."
+
  
     return f"""
-    Output Results as per the Guidelines
+    Context:
     
-    You are a Human Resource Manager in a Software Company. Your main job is to help a person to find matching employees that would fit his needs. 
-    You should ask for more information about what type of work the manager wants, but you should not exceed the information given in the context.
-    Given an input question, first create a syntactically correct MySQL query to run, then look at the results of the query and return the answer.
-    Use the following format:
+    You are provided with a MySQL database structure consisting of one or more tables. 
+    Each table includes specific columns and their data types. Your task is to analyze the provided context, understand the structure of the tables, and write an SQL query to answer a 
+    specific question posed by the user. Given an input question, first create a syntactically correct MySQL query to run, then look at the results of the query and return the answer.
+    Please make sure to not exceed the information given in the context.
     
-    Question: "User question here"
-    SQLQuery: "SQL Query used to generate the result (if applicable)"
-    SQLResult: "Result of the SQLQuery (if applicable)"
-    Answer: "Final answer here (You fill this in with the SQL query only)"
-    
-    Only use the following tables and columns:
+    Table Structure:
     
     {table_prompt}
-    {history}
     
-    Question: "{question}"
-    {query_prompt}
+    User's Question:
     
-    {rules}
+    {question}
+    
+    Tasks:
+    
+    1. Analyze the provided table structure, understand the relationships and data contained within SQL Results.
+    2. Write an syntactically correct SQL query that answers the user's question.
+    
+    Deliverables:
+    
+    The syntactically correct SQL query that was generated.          
     """
 
 def query_gemini_ai(prompt):
@@ -96,8 +109,10 @@ def query_gemini_ai(prompt):
     return response.text
 
 def get_query(question): 
-    prompt = build_prompt(question)
+    prompt = build_query_prompt(question)
     query = query_gemini_ai(prompt)
+    query = query.replace('```sql','')
+    query = query.replace('```','')
     query = query.strip()
  
     query_safety_check(query)
@@ -119,7 +134,7 @@ def get_query_results_from_database(query):
  
     return json.dumps(employees, indent=4, default=str)
  
-def generate_response(question, chat_messages=None):
+def generate_response(question):
     query = get_query(question)
     # print("*" * 50 + "QUERY" + "*" * 50)
     # print(query)
@@ -128,7 +143,7 @@ def generate_response(question, chat_messages=None):
     # print("*" * 50 + "QUERY OUTPUT" + "*" * 50)
     # print(query_output)
     # print("*" * 50 + "QUERY OUTPUT" + "*" * 50)
-    retry_query_prompt = build_prompt(question, query, query_output, chat_messages=chat_messages)
+    retry_query_prompt = build_answer_prompt(question, query, query_output)
     # print("*" * 50 + "RETRY QUERY PROMPT" + "*" * 50)
     # print(retry_query_prompt)
     # print("*" * 50 + "RETRY QUERY PROMPT" + "*" * 50)
